14ecc743bcf720cd99573136f8ef173d5c4254f5
mohammadfaramarzi1 <faramarzi.dev@gamil.com>
2024-11-29 10:30:07 +0330

    feat: complete logic accessToken and refreshToken request

--- src/configs/api.js
+++ src/configs/api.js
@@ -1,5 +1,7 @@
 import axios from "axios";
-import { getCookie } from "utils/cookie";
+
+import { getNewTokens } from "services/token";
+import { getCookie, setCookie } from "utils/cookie";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
@@ -25,12 +27,15 @@ api.interceptors.response.use(
   (response) => {
     return response;
   },
-  (error) => {
+  async (error) => {
     const originalRequest = error.config;
     console.log({ originalRequest, error });
     if (error.response.status === 401 && !originalRequest._retry) {
       originalRequest._retry = true;
-      
+      const res = await getNewTokens();
+      if (!res?.response) return;
+      setCookie(res.response.data);
+      return api(originalRequest);
     }
   }
 );
