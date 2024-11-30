3e2ee062694946dc7bf5a295564ad19047aa95c1
mohammadfaramarzi1 <faramarzi.dev@gamil.com>
2024-11-30 07:27:39 +0330

    style: Layout.module.css

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,11 +1,13 @@
 import Footer from "layouts/Footer";
 import Header from "layouts/Header";
 
+import styles from "layouts/Layout.module.css"
+
 function Layout({ children }) {
   return (
     <>
       <Header />
-      <div>{children}</div>
+      <div className={styles.main}>{children}</div>
       <Footer />
     </>
   );
