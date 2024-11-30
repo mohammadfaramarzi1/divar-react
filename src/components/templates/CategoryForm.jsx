352080ce2503b3e30e4997878dddb3d5266bb7e1
mohammadfaramarzi1 <faramarzi.dev@gamil.com>
2024-11-30 08:01:48 +0330

    feat: handle logic changeHandler in CategoryForm.jsx

--- src/components/templates/CategoryForm.jsx
+++ src/components/templates/CategoryForm.jsx
@@ -1,16 +1,47 @@
+import { useState } from "react";
+
 function CategoryForm() {
+  const [form, setForm] = useState({
+    name: "",
+    slug: "",
+    icon: "",
+  });
+
+  const changeHandler = (event) => {
+    setForm({ ...form, [event.target.name]: event.target.value });
+  };
+
+
   return (
-    <form>
-        <h3>دسته بندی جدید</h3>
-        <label htmlFor="name">نام دسته بندی</label>
-        <input type="text" name="name" id="name" placeholder="نام" />
-        <label htmlFor="slug">اسلاگ دسته بندی</label>
-        <input type="text" name="slug" id="slug" placeholder="اسلاگ" />
-        <label htmlFor="icon">آیکون دسته بندی</label>
-        <input type="text" name="icon" id="icon" placeholder="آیکون" />
-        <button>ایجاد</button>
+    <form onChange={changeHandler} onSubmit={submitHandler}>
+      <h3>دسته بندی جدید</h3>
+      <label htmlFor="name">نام دسته بندی</label>
+      <input
+        type="text"
+        name="name"
+        id="name"
+        placeholder="نام"
+        value={form.name}
+      />
+      <label htmlFor="slug">اسلاگ دسته بندی</label>
+      <input
+        type="text"
+        name="slug"
+        id="slug"
+        placeholder="اسلاگ"
+        value={form.slug}
+      />
+      <label htmlFor="icon">آیکون دسته بندی</label>
+      <input
+        type="text"
+        name="icon"
+        id="icon"
+        placeholder="آیکون"
+        value={form.icon}
+      />
+      <button type="submit">ایجاد</button>
     </form>
-  )
+  );
 }
 
-export default CategoryForm
+export default CategoryForm;
