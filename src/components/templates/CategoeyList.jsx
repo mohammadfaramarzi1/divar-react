97f9267dc8182c755c4542a18b66a674676e1220
mohammadfaramarzi1 <faramarzi.dev@gamil.com>
2024-12-01 13:56:31 +0330

    feat: CategoryList.jsx template and get all categories

--- src/components/templates/CategoeyList.jsx
+++ src/components/templates/CategoeyList.jsx
@@ -1,12 +1,28 @@
-import { useQuery } from "@tanstack/react-query"
+import { useQuery } from "@tanstack/react-query";
+
+import { getCategories } from "services/admin";
+import Loader from "../modules/Loader";
 
 function CategoeyList() {
+  const { data, isLoading, error } = useQuery(
+    ["get-categories"],
+    getCategories
+  );
+  console.log({ data, isLoading, error });
 
-    const {data, isLoading, error} = useQuery(["get-categories"])
-
-  return (
-    <div>CategoeyList</div>
-  )
+  return <div>
+    {
+        isLoading ? <Loader /> : 
+            data.data.map(category => (
+                <div key={category._id}>
+                    <img src={`${category.icon}.svg`} alt="icon" />
+                    <h5>{category.name}</h5>
+                    <p>اسلاگ: {category.slug}</p>
+                </div>
+            ))
+        
+    }
+  </div>;
 }
 
-export default CategoeyList
+export default CategoeyList;
