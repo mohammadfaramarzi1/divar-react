d177b74421b548a353ab507f8a58d7e32643581e
mohammadfaramarzi1 <faramarzi.dev@gamil.com>
2024-12-02 11:31:01 +0330

    feat: handle logic getMyPost api

--- src/components/templates/PostList.jsx
+++ src/components/templates/PostList.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import { useQuery } from "@tanstack/react-query";
+
+import { getMyPosts } from "services/user";
+
+import styles from "./PostList.module.css";
 
 function PostList() {
-  return (
-    <div>PostList</div>
-  )
+//   const { data, isLoading, error } = useQuery(["my-post-list"], getMyPosts);
+
+//   console.log({data, isLoading, error})
+
+  return <div>
+    <h3>آگهی های شما</h3>
+
+  </div>;
 }
 
-export default PostList
+export default PostList;
--- src/services/user.js
+++ src/services/user.js
@@ -1,8 +1,7 @@
 import api from "configs/api";
-import { getCookie } from "src/utils/cookie";
-
-const token = getCookie("accessToken");
 
 const getProfile = () => api.get("user/whoami").then((res) => res || false);
 
-export { getProfile };
+const getMyPosts = () => api.get("/post/my");
+
+export { getProfile, getMyPosts };
