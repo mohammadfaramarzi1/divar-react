043c03e282bb7a8b73b48d8eef9be0b75d9a90cf
mohammadfaramarzi1 <faramarzi.dev@gamil.com>
2024-12-02 07:36:04 +0330

    feat: handle deleteCategory mutation

--- src/components/templates/CategoeyList.jsx
+++ src/components/templates/CategoeyList.jsx
@@ -1,31 +1,48 @@
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 
 import Loader from "../modules/Loader";
-import { getCategories } from "services/admin";
+import { deleteCategory, getCategories } from "services/admin";
+import { toast, ToastContainer } from "react-toastify";
 
+import "react-toastify/dist/ReactToastify.css";
 import styles from "./CategoryList.module.css";
 
 function CategoeyList() {
-  const { data, isLoading, error } = useQuery(
+  const { data, isLoading, error, refetch } = useQuery(
     ["get-categories"],
     getCategories
   );
   console.log({ data, isLoading, error });
 
-  return <div className={styles.list}>
-    {
-        isLoading ? <Loader /> : 
-            data.data.map(category => (
-                <div key={category._id}>
-                    <img src={`${category.icon}.svg`} alt="icon" />
-                    <h5>{category.name}</h5>
-                    <button>حذف دسته بندی</button>
-                    <p>اسلاگ: {category.slug}</p>
-                </div>
-            ))
-        
-    }
-  </div>;
+  const { mutate } = useMutation(deleteCategory);
+
+  return (
+    <div className={styles.list}>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        data.data.map((category) => (
+          <div key={category._id}>
+            <img src={`${category.icon}.svg`} alt="icon" />
+            <h5>{category.name}</h5>
+            <button
+              onClick={() => {
+                mutate(category._id);
+                toast("دسته بندی با موفقیت حذف شد.", {
+                  position: "top-right",
+                  type: "success",
+                });
+                refetch();
+              }}
+            >
+              حذف دسته بندی
+            </button>
+            <p>اسلاگ: {category.slug}</p>
+          </div>
+        ))
+      )}
+    </div>
+  );
 }
 
 export default CategoeyList;
