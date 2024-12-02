cebc68b3203158b7366915d2d4bf582336e95cb1
mohammadfaramarzi1 <faramarzi.dev@gamil.com>
2024-12-02 11:53:52 +0330

    style: PostList.module.css

--- src/components/templates/PostList.jsx
+++ src/components/templates/PostList.jsx
@@ -9,32 +9,35 @@ import styles from "./PostList.module.css";
 function PostList() {
   const { data, isLoading, error } = useQuery(["my-post-list"], getMyPosts);
 
-  console.log({data, isLoading, error})
+  console.log({ data, isLoading, error });
 
-  return <div>
-    {
-        isLoading ? <Loader /> : (
-            <>
-                <h3>آگهی های شما</h3>
-                {
-                    data.data.posts.map(post => (
-                        <div key={post._id}>
-                            <img src={`${import.meta.env.VITE_BASE_URL}${post.images[0]}`} alt="post" />
-                            <div>
-                                <p>{post.options.title}</p>
-                                <span>{post.options.content}</span>
-                            </div>
-                            <div>
-                                <p>{new Date(post.createdAt).toLocaleDateString("fa-IR")}</p>
-                                <span>{sp(post.amount)} تومان</span>
-                            </div>
-                        </div>
-                    ))
-                }
-            </>
-        )
-    }
-  </div>;
+  return (
+    <div className={styles.list}>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <>
+          <h3>آگهی های شما</h3>
+          {data.data.posts.map((post) => (
+            <div key={post._id} className={styles.post}>
+              <img
+                src={`${import.meta.env.VITE_BASE_URL}${post.images[0]}`}
+                alt="post"
+              />
+              <div>
+                <p>{post.options.title}</p>
+                <span>{post.options.content}</span>
+              </div>
+              <div className={styles.price}>
+                <p>{new Date(post.createdAt).toLocaleDateString("fa-IR")}</p>
+                <span>{sp(post.amount)} تومان</span>
+              </div>
+            </div>
+          ))}
+        </>
+      )}
+    </div>
+  );
 }
 
 export default PostList;
