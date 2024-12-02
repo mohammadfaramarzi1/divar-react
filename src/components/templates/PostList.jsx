998af3c457d06a76ff76b8bb0f5e638c4f01d84b
mohammadfaramarzi1 <faramarzi.dev@gamil.com>
2024-12-02 11:40:44 +0330

    feat: PostList.jsx template"

--- src/components/templates/PostList.jsx
+++ src/components/templates/PostList.jsx
@@ -1,5 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 
+import Loader from "../modules/Loader";
 import { getMyPosts } from "services/user";
 
 import styles from "./PostList.module.css";
@@ -10,8 +11,28 @@ function PostList() {
   console.log({data, isLoading, error})
 
   return <div>
-    <h3>آگهی های شما</h3>
-
+    {
+        isLoading ? <Loader /> : (
+            <>
+                <h3>آگهی های شما</h3>
+                {
+                    data.data.posts.map(post => (
+                        <div key={post._id}>
+                            <img src={`${import.meta.env.VITE_BASE_URL}${post.images[0]}`} alt="post" />
+                            <div>
+                                <p>{post.options.title}</p>
+                                <span>{post.options.content}</span>
+                            </div>
+                            <div>
+                                <p>{post.createdAt}</p>
+                                <span>{post.amount} تومان</span>
+                            </div>
+                        </div>
+                    ))
+                }
+            </>
+        )
+    }
   </div>;
 }
 
