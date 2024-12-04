d378ef1f454b363f3922dbb35a82aaa3a75277cb
mohammadfaramarzi1 <faramarzi.dev@gamil.com>
2024-12-04 07:52:27 +0330

    style: Sidebar.module.css

--- src/components/templates/Sidebar.jsx
+++ src/components/templates/Sidebar.jsx
@@ -2,11 +2,13 @@ import { useQuery } from "@tanstack/react-query";
 
 import { getCategories } from "services/admin";
 
+import styles from "./Sidebar.module.css"
+
 function Sidebar() {
   const { data } = useQuery(["get-categories"], getCategories);
   console.log(data)
 
-  return <div>
+  return <div className={styles.sidebar}>
     <h4>دسته بندی ها</h4>
     <ul>
       {
