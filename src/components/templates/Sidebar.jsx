64f4fea8c9ae4e059b402f561d34f8ef6e4d2f57
mohammadfaramarzi1 <faramarzi.dev@gamil.com>
2024-12-04 08:12:47 +0330

    feat: handle isLoading logic for getPosts

--- src/components/templates/Sidebar.jsx
+++ src/components/templates/Sidebar.jsx
@@ -1,26 +1,19 @@
-import { useQuery } from "@tanstack/react-query";
+import styles from "./Sidebar.module.css";
 
-import { getCategories } from "services/admin";
-
-import styles from "./Sidebar.module.css"
-
-function Sidebar() {
-  const { data } = useQuery(["get-categories"], getCategories);
-  console.log(data)
-
-  return <div className={styles.sidebar}>
-    <h4>دسته بندی ها</h4>
-    <ul>
-      {
-        data?.data.map(category => (
+function Sidebar({ categories }) {
+  return (
+    <div className={styles.sidebar}>
+      <h4>دسته بندی ها</h4>
+      <ul>
+        {categories.data.map((category) => (
           <li key={category._id}>
             <img src={`${category.icon}.svg`} alt={category.name} />
             <p>{category.name}</p>
           </li>
-        ))
-      }
-    </ul>
-  </div>;
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default Sidebar;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,21 +4,26 @@ import Main from "components/templates/Main";
 import Sidebar from "components/templates/Sidebar";
 import Loader from "components/modules/Loader";
 import { getAllPosts } from "services/user";
-
+import { getCategories } from "services/admin";
 
 function HomePage() {
-  const { data, isLoading } = useQuery(["post-list"], getAllPosts);
-
-  console.log(data);
+  const { data: posts, isLoading: isLoadingPost } = useQuery(
+    ["post-list"],
+    getAllPosts
+  );
+  const { data: categories, isLoading: isLoadingCategory } = useQuery(
+    ["get-categories"],
+    getCategories
+  );
 
   return (
     <>
-      {isLoading ? (
+      {isLoadingCategory || isLoadingPost ? (
         <Loader />
       ) : (
         <div style={{ display: "flex" }}>
-          <Sidebar />
-          <Main posts={data} />
+          <Sidebar categories={categories} />
+          <Main posts={posts} />
         </div>
       )}
     </>
