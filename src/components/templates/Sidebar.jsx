17290bc9e802f53422472b9bb7c3012d3f562429
mohammadfaramarzi1 <faramarzi.dev@gamil.com>
2024-12-04 07:47:06 +0330

    feat: Sidebar.jsx template

--- src/components/templates/Sidebar.jsx
+++ src/components/templates/Sidebar.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import { useQuery } from "@tanstack/react-query";
+
+import { getCategories } from "services/admin";
 
 function Sidebar() {
-  return (
-    <div>Sidebar</div>
-  )
+  const { data } = useQuery(["get-categories"], getCategories);
+  console.log(data)
+
+  return <div>
+    <h4>دسته بندی ها</h4>
+    <ul>
+      {
+        data?.data.map(category => (
+          <li key={category._id}>
+            <img src={`${category.icon}.svg`} alt={category.name} />
+            <p>{category.name}</p>
+          </li>
+        ))
+      }
+    </ul>
+  </div>;
 }
 
-export default Sidebar
+export default Sidebar;
