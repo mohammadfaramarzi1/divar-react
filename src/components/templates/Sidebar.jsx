abde96081520640a19bcfdef20ad2949c87c6549
mohammadfaramarzi1 <faramarzi.dev@gamil.com>
2024-12-06 19:23:50 +0330

    feat: searchParams for categoryId

--- src/components/templates/Sidebar.jsx
+++ src/components/templates/Sidebar.jsx
@@ -1,12 +1,20 @@
+import { useSearchParams } from "react-router-dom";
 import styles from "./Sidebar.module.css";
 
 function Sidebar({ categories }) {
+  const [searchParams, setSearchParams] = useSearchParams();
+  console.log({ searchParams, setSearchParams });
+
+  const categoryHandler = (categoryId) => {
+    setSearchParams({ categoryId });
+  };
+
   return (
     <div className={styles.sidebar}>
       <h4>دسته بندی ها</h4>
       <ul>
         {categories.data.map((category) => (
-          <li key={category._id}>
+          <li key={category._id} onClick={() => categoryHandler(category._id)}>
             <img src={`${category.icon}.svg`} alt={category.name} />
             <p>{category.name}</p>
           </li>
